72edee9bd49412b192547f0bd67169473ab3100d
bradleymarch <bradleymarch@gmail.com>
2017-08-17 10:53:00 -0500

    css

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function myFunction() {
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var dropdowns = document.getElementsByClassName("dropdownContent");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
